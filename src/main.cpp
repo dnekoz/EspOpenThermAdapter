db2e174a6aaadf50a7e8903326e0804ddffc5d8d
Denis <dnekoz@gmail.com>
2020-09-25 20:11:16 +0300

    --

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,25 @@
-#include <Arduino.h>
+#include "Wire.h"
+#include <PubSubClient.h>
+#include <ESP8266WiFi.h>
 
-void setup() {
-  // put your setup code here, to run once:
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+
+  WiFi.begin("network-name", "pass-to-network");
+
+  Serial.print("Connecting");  //  "Подключение"
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.print("Connected, IP address: ");
+           //  "Подключились, IP-адрес: "
+  Serial.println(WiFi.localIP());
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop() {}
